ecbd968645e9851c071ae7c023f955ab139cefae
Joe Masilotti <joe@masilotti.com>
2026-02-11 22:05:37 -0800

    Allow Cloudflare tunnels to access dev server

--- turbo/config/environments/development.rb
+++ turbo/config/environments/development.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions.
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Allow Cloudflare tunnels to access development server.
+  config.hosts << ".trycloudflare.com"
 end
